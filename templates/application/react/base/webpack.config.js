8d6738f0ca51d423191b6227067c8f581573fa85
DemonDis <50297797+DemonDis@users.noreply.github.com>
2022-12-18 21:30:32 +0700

    :construction: weboack production

--- templates/application/react/base/webpack.config.js
+++ templates/application/react/base/webpack.config.js
@@ -1,14 +1,21 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-const deps = require("./package.json").dependencies;
-module.exports = {
+const webpack = require('webpack');
+const path = require('path');
+const deps = require('./package.json').dependencies;
+
+module.exports = (env, arg) => ({
+  module: arg.mode === 'production' ? 'production' : 'development',
+  devtool: arg.mode === 'production' ? 'source-map' : 'eval',
+  entry: './src/index.js',
+
   output: {
-    publicPath: "http://localhost:{{PORT}}/",
+    publicPath: env.hasOwnProperty('WEBPACK_SERVE') ? 'http://localhost:{{PORT}}/' : '/{{SAFE_NAME}}/',
   },
 
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
     alias: {
       'react-dom': '@hot-loader/react-dom'
     }
@@ -17,31 +24,31 @@ module.exports = {
   devServer: {
     port: {{PORT}},
     historyApiFallback: true,
-    headers: {"Access-Control-Allow-Origin": "*"}
+    headers: {'Access-Control-Allow-Origin': '*'}
   },
 
   module: {
     rules: [
       {
         test: /\.m?js/,
-        type: "javascript/auto",
+        type: 'javascript/auto',
         resolve: {
           fullySpecified: false,
         },
       },
       {
         test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
     ],
@@ -49,25 +56,33 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "{{SAFE_NAME}}",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {},
+      name: '{{SAFE_NAME}}',
+      filename: 'remoteEntry.js',
+      remotes: {
+        /* Example remotes
+          components: `components@{env.hasOwnProperty('WEBPACK_SERVE') ? 'http://localhost:{{PORT}}/' : '/{{SAFE_NAME}}'}remoteEntry.js`,
+        */
+      },
+      exposes: {
+        /* Example exposes
+          './Button': './src/components/Button.jsx',
+        */
+      },
       shared: {
         ...deps,
         react: {
           singleton: true,
           requiredVersion: deps.react,
         },
-        "react-dom": {
+        'react-dom': {
           singleton: true,
-          requiredVersion: deps["react-dom"],
+          requiredVersion: deps['react-dom'],
         },
       },
     }),
     new HtmlWebPackPlugin({
-      template: "./src/public/index.html",
+      template: './src/public/index.html',
       favicon: './src/public/favicon.ico',
     }),
   ],
-};
+});
---
+++ templates/packages/packages/react/js/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { hot } from 'react-hot-loader/root';
+
+import "./index.{{CSS_EXTENSION}}";
+
+const App = () => (
+  <div className="{{CONTAINER}}">
+    <div>Name: {{ NAME }}</div>
+    <div>Framework: {{ FRAMEWORK }}</div>
+    <div>Language: {{ LANGUAGE }}</div>
+    <div>CSS: {{ CSS }}</div>
+  </div>
+);
+
+export default hot(App);
