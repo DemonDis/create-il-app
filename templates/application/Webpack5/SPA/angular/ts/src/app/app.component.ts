30d816abc89d1b60a02c2d1020a1edee54f33d29
DemonDis <50297797+DemonDis@users.noreply.github.com>
2023-01-08 20:38:57 +0700

    :construction: angular module federation

--- templates/application/Webpack5/SPA/angular/ts/src/app/app.component.ts
+++ templates/application/Webpack5/SPA/angular/ts/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  styleUrls: ['../styles/index.css']
 })
 export class AppComponent {
-  title = 'my-app-ang';
+  title = "{{SAFE_NAME}}";
 }
