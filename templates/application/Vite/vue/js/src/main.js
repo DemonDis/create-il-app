8d7969edf094bc8f737a61b322a4e9960176a1f3
DemonDis <dimas.ras@mail.ru>
2023-03-06 21:32:43 +0700

    🔥 only react

--- templates/application/Vite/vue/js/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-// import webpackContent from 'webpack-side/Content';
-const app = createApp(App);
-
-// app.component('webpack-content', webpackContent);
-
-app.mount('#app');
